c8b291f9b1fd3ebed32c7f84a4a21b53447be222
Alexey Chernenko <alexey@nexgent.com>
2021-09-07 19:10:31 +0300

    cover all code

--- src/Math.Tests/MathTests.cs
+++ src/Math.Tests/MathTests.cs
@@ -10,5 +10,14 @@ namespace Math.Tests
 
         [Fact]
         public void Should_Add() => Assert.Equal(3, _service.AddOrSubtract(1, 2, true));
+
+        [Fact]
+        public void Should_Subtract() => Assert.Equal(1, _service.AddOrSubtract(2, 1, false));
+
+        [Fact]
+        public void Should_Multiply() => Assert.Equal(4, _service.MultiplyOfDivide(2, 2, true));
+
+        [Fact]
+        public void Should_Divide() => Assert.Equal(2, _service.MultiplyOfDivide(4, 2, false));
     }
 }
